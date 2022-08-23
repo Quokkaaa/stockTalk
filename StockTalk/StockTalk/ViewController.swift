e00d46273e3c510848c864ec68f0bdad046dfa38
Quokkaa <rktrkdml@gmail.com>
2022-08-23 11:03:19 +0900

    fix: Main storyboard 제거 및 초기화면 셋팅

--- StockTalk/StockTalk/ViewController.swift
+++ StockTalk/StockTalk/ViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .blue
   }
-
-
 }
-
+/*
+ 
+ 기능
+ - 로그인 { 회원가입(sns로그인), 회원탈퇴 }
+ - 글목록 (추가,수정,삭제) 이미지갯수, 동영상, 상세페이지, 신고기능
+ - optional(댓글, 수정,삭제,등록,신고)
+ - 회원관리(설정, 로그아웃)
+ - 채팅
+ 
+ 
+ */
