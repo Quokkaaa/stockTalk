6326c369f1642d082d6d41d7febd48cbd30771b5
Kaui <91132536+Quokkaaa@users.noreply.github.com>
2022-08-23 10:39:56 +0900

    file: add project

---
+++ StockTalk/StockTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockTalk
+//
+//  Created by LIMGAUI on 2022/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
