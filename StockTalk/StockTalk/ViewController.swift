f09fdb177201422f968ecbe8507df634ef00bf38
Quokkaa <rktrkdml@gmail.com>
2022-08-23 11:03:34 +0900

    style: 주석제거

--- StockTalk/StockTalk/ViewController.swift
+++ StockTalk/StockTalk/ViewController.swift
@@ -13,14 +13,3 @@ final class ViewController: UIViewController {
     view.backgroundColor = .blue
   }
 }
-/*
- 
- 기능
- - 로그인 { 회원가입(sns로그인), 회원탈퇴 }
- - 글목록 (추가,수정,삭제) 이미지갯수, 동영상, 상세페이지, 신고기능
- - optional(댓글, 수정,삭제,등록,신고)
- - 회원관리(설정, 로그아웃)
- - 채팅
- 
- 
- */
