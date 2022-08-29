65a85236d90e5285965c22e4293b97308e4d6f9f
Quokkaa <rktrkdml@gmail.com>
2022-08-29 19:53:48 +0900

    refactor: viewController와 view를 분리

--- StockTalk/StockTalk/Scene/Login/View/LoginView.swift
+++ StockTalk/StockTalk/Scene/Login/View/LoginView.swift
@@ -7,14 +7,91 @@
 
 import UIKit
 
-class LoginView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class LoginView: UIView {
+  private let stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.spacing = 150
+    stackView.axis = .vertical
+    stackView.alignment = .center
+    stackView.distribution = .fill
+    return stackView
+  }()
+  
+  private let titleStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.spacing = 5
+    stackView.axis = .vertical
+    stackView.alignment = .center
+    stackView.distribution = .fill
+    return stackView
+  }()
+  
+  let titleLabel: UILabel = {
+    let label = UILabel()
+    label.text = "StockTalk"
+    label.textAlignment = .center
+    label.font = UIFont.boldSystemFont(ofSize: 50)
+    return label
+  }()
+  
+  private let subtitleLabel: UILabel = {
+    let label = UILabel()
+    label.text = "해당 서비스 이용을 위해 로그인 해주세요."
+    label.textAlignment = .center
+    label.textColor = .systemGray3
+    return label
+  }()
+  
+  private let buttonStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.spacing = 20
+    stackView.axis = .vertical
+    stackView.alignment = .center
+    stackView.distribution = .fill
+    return stackView
+  }()
+  
+  lazy var kakaoLoginButton: UIButton = {
+    let button = UIButton()
+    button.setImage(UIImage(named: "kakao_login_medium_wide"), for: .normal)
+    return button
+  }()
+  
+  lazy var appleLoginButton: UIButton = {
+    let button = UIButton()
+    button.backgroundColor = .black
+    button.setTitle(
+        "                       Apple 로그인                        ",
+        for: .normal)
+    button.setTitleColor(UIColor.white, for: .normal)
+    button.setImage(UIImage(named: "apple_logo_medium"), for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+    button.layer.cornerRadius = 5
+    return button
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .systemBackground
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configureUI() {
+    addSubview(stackView)
+    
+    titleStackView.addArrangedSubview(titleLabel)
+    titleStackView.addArrangedSubview(subtitleLabel)
+    buttonStackView.addArrangedSubview(kakaoLoginButton)
+    buttonStackView.addArrangedSubview(appleLoginButton)
+    stackView.addArrangedSubview(titleStackView)
+    stackView.addArrangedSubview(buttonStackView)
+    
+    stackView.snp.makeConstraints { make in
+      make.center.equalTo(self)
     }
-    */
-
+  }
 }
--- StockTalk/StockTalk/Scene/Login/View/LoginViewController.swift
+++ StockTalk/StockTalk/Scene/Login/View/LoginViewController.swift
@@ -12,99 +12,29 @@ import AuthenticationServices
 final class LoginViewController: UIViewController {
   private let loginViewModel = LoginViewModel()
   private var subscriptions = Set<AnyCancellable>()
+  private let loginView = LoginView(frame: .zero)
   
-  private let stackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.spacing = 150
-    stackView.axis = .vertical
-    stackView.alignment = .center
-    stackView.distribution = .fill
-    return stackView
-  }()
-  
-  private let titleStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.spacing = 5
-    stackView.axis = .vertical
-    stackView.alignment = .center
-    stackView.distribution = .fill
-    return stackView
-  }()
-  
-  private let titleLabel: UILabel = {
-    let label = UILabel()
-    label.text = "StockTalk"
-    label.textAlignment = .center
-    label.font = UIFont.boldSystemFont(ofSize: 50)
-    return label
-  }()
-  
-  private let subtitleLabel: UILabel = {
-    let label = UILabel()
-    label.text = "해당 서비스 이용을 위해 로그인 해주세요."
-    label.textAlignment = .center
-    label.textColor = .systemGray3
-    return label
-  }()
-  
-  private let buttonStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.spacing = 20
-    stackView.axis = .vertical
-    stackView.alignment = .center
-    stackView.distribution = .fill
-    return stackView
-  }()
-  
-  private lazy var kakaoButton = { (action: Selector) -> UIButton in
-    let button = UIButton()
-    button.setImage(UIImage(named: "kakao_login_medium_wide"), for: .normal)
-    button.addTarget(
-      self, action: action, for: .touchUpInside)
-    return button
+  override func loadView() {
+    super.loadView()
+    view = loginView
   }
-  
-  private lazy var appleLoginButton: UIButton = {
-    let button = UIButton()
-    button.backgroundColor = .black
-    button.setTitle(
-        "                       Apple 로그인                        ",
-        for: .normal)
-    button.setTitleColor(UIColor.white, for: .normal)
-    button.setImage(UIImage(named: "apple_logo_medium"), for: .normal)
-    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
-    button.layer.cornerRadius = 5
-    button.addTarget(
-      self,
-      action: #selector(appleLoginButtonDidTap),
-      for: .touchUpInside)
-    return button
-  }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .systemBackground
-    configureUI()
+    setTargets()
     setBindings()
   }
   
-  private func configureUI() {
-    view.addSubview(stackView)
+  private func setTargets() {
+    loginView.appleLoginButton.addTarget(
+      self,
+      action: #selector(appleLoginButtonDidTap),
+      for: .touchUpInside)
     
-    let kakaoLoginButton = kakaoButton(#selector(kakaoLoginButtonDidTap))
-    
-    appleLoginButton.addTarget(self, action: #selector(appleLoginButtonDidTap), for: .touchUpInside)
-    
-    titleStackView.addArrangedSubview(titleLabel)
-    titleStackView.addArrangedSubview(subtitleLabel)
-    buttonStackView.addArrangedSubview(kakaoLoginButton)
-    buttonStackView.addArrangedSubview(appleLoginButton)
-    stackView.addArrangedSubview(titleStackView)
-    stackView.addArrangedSubview(buttonStackView)
-    
-    stackView.snp.makeConstraints { make in
-      make.center.equalTo(view)
-    }
+    loginView.appleLoginButton.addTarget(
+      self,
+      action: #selector(kakaoLoginButtonDidTap),
+      for: .touchUpInside)
   }
   
   @objc private func kakaoLoginButtonDidTap() {
@@ -128,7 +58,7 @@ private extension LoginViewController {
     // MARK: - 두번째방법 AnyPublisher로 값을 받아서 Label에 꽂아준다.
     self.loginViewModel.loginService?.loginStatusInfo
       .receive(on: DispatchQueue.main)
-      .assign(to: \.text, on: titleLabel)
+      .assign(to: \.text, on: loginView.titleLabel)
       .store(in: &subscriptions)
   }
 }
