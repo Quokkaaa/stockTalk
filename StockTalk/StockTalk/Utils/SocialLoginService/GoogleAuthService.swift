837603e90ef31abc0c0c970e4be282202a8ab274
Quokkaa <rktrkdml@gmail.com>
2022-08-29 20:05:49 +0900

    feat: GoogleAuth file생성

---
+++ StockTalk/StockTalk/Utils/SocialLoginService/GoogleAuthService.swift
@@ -0,0 +1,26 @@
+//
+//  GoogleAuthService.swift
+//  StockTalk
+//
+//  Created by LIMGAUI on 2022/08/29.
+//
+
+import Foundation
+import Combine
+
+final class GoogleAuthService: SocialLoginable {
+  static let shared = GoogleAuthService()
+  
+  @Published var isLoggedIn: Bool = false
+  
+  lazy var loginStatusInfo: AnyPublisher<String?, Never> = $isLoggedIn
+    .compactMap { $0 ? "로그인 상태" : "로그아웃 상태" }.eraseToAnyPublisher()
+  
+  func login() {
+    //
+  }
+  
+  func logout() {
+    //
+  }
+}
